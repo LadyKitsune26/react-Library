55240c1ff00a334a1a65ecb26f7f58a30cf8a59f
LadyKitsune26 <langlois.raimey@gmail.com>
2025-10-14 20:58:37 -0500

    done for the day

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,20 +1,51 @@
-import React from "react";
-import Book from "./Book";
-import BestBooks from "./ui/BestBooks";
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Book from './Book';
+import { books } from '../data'
 
-const Featured = () => {
-  return (
-    <section id="features">
+ const Featured = () => {
+  console.log(books)
+  return(
+    <section id='features'>
       <div className="container">
         <div className="row">
           <h2 className="section__title">
             Featured <span className="purple">Books</span>
           </h2>
-          <BestBooks />
+          <div className="books">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+          </div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+ }
 
-export default Featured;
+ export default Featured
+
+
+
+
+// import React from "react";
+// import Book from "./Book";
+// import BestBooks from "./ui/BestBooks";
+
+// const Featured = () => {
+//   return (
+//     <section id="features">
+//       <div className="container">
+//         <div className="row">
+//           <h2 className="section__title">
+//             Featured <span className="purple">Books</span>
+//           </h2>
+//           <BestBooks />
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Featured;
